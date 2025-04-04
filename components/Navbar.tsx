4e222eb0a00ad4797fc50d2d9a164a57db72162c
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-04 14:48:45 -0500

    add styling to navbar items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,19 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="sticky top-0 z-50 bg-white shadow">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4">
         <Link href="/">Clothes Minded</Link>
-      </div>
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/products">Products</Link>
-        <Link href="/checkout">Checkout</Link>
+        <div className="hidden md:flex space-x-6">
+          <Link href="/">Home</Link>
+          <Link href="/products" className="hover:text-blue-600">
+            Products
+          </Link>
+          <Link href="/checkout" className="hover:text-blue-600">
+            Checkout
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4"></div>
       </div>
     </nav>
   );
