9e6568b9bafc260e873f4c1518ceb9084528e099
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-05 16:32:08 -0500

    add handlers onto input

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -26,6 +26,8 @@ const ProductList = ({ products }: ProductListProps) => {
         <input
           type="text"
           placeholder="Search produts"
+          value={userInput}
+          onChange={(e) => setuserInput(e.target.value)}
           className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
