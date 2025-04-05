0a8e969880eb49f0cd82775a30ae7ff9f76d7bcc
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-05 16:22:43 -0500

    add styling on parent page component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,8 +7,10 @@ const ProductsPage = async () => {
     expand: ["data.default_price"],
   });
   return (
-    <div>
-      <h1>All products</h1>
+    <div className="pb-8">
+      <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+        All products
+      </h1>
       <ProductList products={products.data} />
     </div>
   );
