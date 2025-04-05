1b012c97c24821afce62ad6010a980b04329295a
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-05 16:42:41 -0500

    fix typo

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps {
 const ProductCard = ({ product }: ProductCardProps) => {
   const price = product.default_price as Stripe.Price;
   return (
-    <Link href={`/procuts/${product.id}`} className="block h-full">
+    <Link href={`/products/${product.id}`} className="block h-full">
       <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-gray-300 gap-0">
         {product.images && product.images[0] && (
           <div className="relative h-80 w-full">
