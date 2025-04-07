84b64190d69636d5279297e45656ea722d5049af
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-04-07 16:42:02 -0500

    bring in increase and reduce handler from useCartStore hook

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,58 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useCartStore } from "@/store/CartStore";
 import React from "react";
 
 const CheckoutPage = () => {
-  return <div>Checkout</div>;
+  const { items, addItem, removeItem } = useCartStore();
+  const total = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+  if (total === 0 || items.length === 0) {
+    return (
+      <div>
+        <h1>Your cart is empty.</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Order summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {items.map((item, key) => (
+              <li key={key}>
+                <div>
+                  <span>{item.name}</span>
+                  <span>
+                    ${((item.price * item.quantity) / 100).toFixed(2)}
+                  </span>
+                </div>
+                <div>
+                  <Button variant="outline" onClick={() => removeItem(item.id)}>
+                    -
+                  </Button>
+                  <span className="text-lg font-semibold">{item.quantity}</span>
+                  <Button
+                    variant="outline"
+                    onClick={() => addItem({ ...item, quantity: 1 })}
+                  >
+                    +
+                  </Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default CheckoutPage;
